c12d5a23e1f62e26988f7f277aead89f03fc74e5
RoniDaou <roni.r.daou@gmail.com>
2026-07-12 21:15:52 +0300

    .

--- frontend/src/components/BlogBox.js
+++ frontend/src/components/BlogBox.js
@@ -1,29 +1,44 @@
 import React from "react";
 import months from "../data/months";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import useAuthContext from "../hooks/useAuthContext";
 import { LatestBlogsContext } from "../context/LatestBlogsContext";
 import { PopularBlogsContext } from "../context/PopularBlogsContext";
-import { useNavigate } from "react-router-dom";
 
 export default function BlogBox({ blog }) {
   const { title, author, category, datePublished } = blog;
-  const date = new Date(datePublished);
   const { user } = useAuthContext();
+
   const { dispatch: latestBlogsDispatch } =
     React.useContext(LatestBlogsContext);
+
   const { dispatch: popularBlogsDispatch } =
     React.useContext(PopularBlogsContext);
 
-  const year = date.getYear() + 1900;
+  const navigate = useNavigate();
+
+  const date = new Date(datePublished);
+  const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
 
-  const navigate = useNavigate();
+  const likedBy = blog.likedby || [];
+  const dislikedBy = blog.dislikedby || [];
+
+  const userId = user?.id || user?._id;
+
+  const userLiked = userId
+    ? likedBy.some((id) => String(id) === String(userId))
+    : false;
+
+  const userDisliked = userId
+    ? dislikedBy.some((id) => String(id) === String(userId))
+    : false;
 
   async function handleLike(event) {
     event.preventDefault();
+    event.stopPropagation();
 
     if (!user) {
       navigate("/signInUp");
@@ -31,7 +46,7 @@ export default function BlogBox({ blog }) {
     }
 
     try {
-      const response = await axios.post(
+      await axios.post(
         "/blogs/like",
         {
           _id: blog._id,
@@ -43,18 +58,15 @@ export default function BlogBox({ blog }) {
         },
       );
 
-      const updatedBlog = response.data.blog || response.data.Blog;
-      const userId = user._id || user.id;
-
       latestBlogsDispatch({
         type: "UPDATE_LIKE",
-        blog_id: updatedBlog._id,
+        blog_id: blog._id,
         user_id: userId,
       });
 
       popularBlogsDispatch({
         type: "UPDATE_LIKE",
-        blog_id: updatedBlog._id,
+        blog_id: blog._id,
         user_id: userId,
       });
     } catch (error) {
@@ -64,6 +76,7 @@ export default function BlogBox({ blog }) {
 
   async function handleDislike(event) {
     event.preventDefault();
+    event.stopPropagation();
 
     if (!user) {
       navigate("/signInUp");
@@ -71,7 +84,7 @@ export default function BlogBox({ blog }) {
     }
 
     try {
-      const response = await axios.post(
+      await axios.post(
         "/blogs/dislike",
         {
           _id: blog._id,
@@ -83,18 +96,15 @@ export default function BlogBox({ blog }) {
         },
       );
 
-      const updatedBlog = response.data.blog || response.data.Blog;
-      const userId = user._id || user.id;
-
       latestBlogsDispatch({
         type: "UPDATE_DISLIKE",
-        blog_id: updatedBlog._id,
+        blog_id: blog._id,
         user_id: userId,
       });
 
       popularBlogsDispatch({
         type: "UPDATE_DISLIKE",
-        blog_id: updatedBlog._id,
+        blog_id: blog._id,
         user_id: userId,
       });
     } catch (error) {
@@ -108,35 +118,46 @@ export default function BlogBox({ blog }) {
         <div
           className="blog-box--img"
           style={{
-            backgroundImage: `url(${blog.image.image})`,
+            backgroundImage: `url(${blog.image?.image})`,
             backgroundPosition: "center",
             backgroundSize: "cover",
           }}
-        ></div>
+        />
+
         <div className="category-likes--container">
           <p className="blog-box-category">{category}</p>
+
           <div className="likes--container">
-            <p>{blog.likedby.length}</p>
+            <p>{likedBy.length}</p>
+
             <span
-              className={`material-symbols-rounded thumb-up${user && blog.likedby.includes(user.id) ? " thumb--clicked" : ""}`}
+              className={`material-symbols-rounded thumb-up${
+                userLiked ? " thumb--clicked" : ""
+              }`}
               onClick={handleLike}
             >
               thumb_up
             </span>
-            <p>{blog.dislikedby.length}</p>
+
+            <p>{dislikedBy.length}</p>
+
             <span
-              className={`material-symbols-rounded thumb-up${user && blog.dislikedby.includes(user.id) ? " thumb--clicked" : ""}`}
+              className={`material-symbols-rounded thumb-up${
+                userDisliked ? " thumb--clicked" : ""
+              }`}
               onClick={handleDislike}
             >
               thumb_down
             </span>
           </div>
         </div>
+
         <h6 className="blog-box-title">{title}</h6>
       </div>
 
       <div className="blog-box--info">
         <p className="blog-box-author">{author}</p>
+
         <p className="blog-box-date">{`${months[month]} ${day}, ${year}`}</p>
       </div>
     </Link>
